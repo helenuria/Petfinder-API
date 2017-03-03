1258d90d50f4d32faf3a02c5277f5205be2ef277
canetoadexplode <helen.n.stockman@gmail.com>
2017-03-02 17:35:56 -0800

    iframe files

---
+++ script.js
@@ -0,0 +1,51 @@
+var apiKey = '2f95f51b181ddd27883e91878e922466'; // assign our key to a variable, easier to read
+
+// the next line and function set up the button in our html to be clickable and reactive 
+document.addEventListener('DOMContentLoaded', bindButtons);
+
+function bindButtons(){
+	document.getElementById('submitZip').addEventListener('click', function(event){
+		event.preventDefault();
+		var zip = document.getElementById('zip').value; // this line gets the zip code from the form entry
+		var url = 'http://api.petfinder.com/pet.getRandom';
+		
+		// Within $.ajax{...} is where we fill out our query 
+		$.ajax({
+			url: url,
+			jsonp: "callback",
+			dataType: "jsonp",
+			data: {
+				key: apiKey,
+				animal: 'cat',
+				'location': zip,
+				output: 'basic',
+				format: 'json'
+			},
+			// Here is where we handle the response we got back from Petfinder
+			success: function( response ) {
+				console.log(response); // debugging
+				var catName = response.petfinder.pet.name.$t;
+				var img = response.petfinder.pet.media.photos.photo[0].$t;
+				var id = response.petfinder.pet.id.$t;
+
+				var newName = document.createElement('a');
+				var newDiv = document.createElement('div');
+				newName.textContent = catName;
+				newName.href = 'https://www.petfinder.com/petdetail/' + id;
+
+				var newImg = document.createElement('img');
+				newImg.src = img;
+				
+				var list = document.createElement("ul");
+				list.setAttribute("id", "List");
+				document.body.appendChild(list);
+
+				newDiv.appendChild(newName);
+				list.appendChild(newDiv);
+				list.appendChild(newImg);
+			}
+		});
+		})
+
+}
+
