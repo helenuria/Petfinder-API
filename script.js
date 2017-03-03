7e92758755152819a2b4d5c9d41c0d4ec0239964
canetoadexplode <helen.n.stockman@gmail.com>
2017-03-02 17:45:40 -0800

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function bindButtons(){
 	document.getElementById('submitZip').addEventListener('click', function(event){
 		event.preventDefault();
 		var zip = document.getElementById('zip').value; // this line gets the zip code from the form entry
-		var url = 'http://api.petfinder.com/pet.getRandom';
+		var url = 'https://api.petfinder.com/pet.getRandom';
 		
 		// Within $.ajax{...} is where we fill out our query 
 		$.ajax({
